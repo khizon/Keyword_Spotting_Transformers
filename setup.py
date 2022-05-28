305c9c34c8ae8cd246927101d4a718897a25a2e9
khizon <kielhizon@gmail.com>
2022-05-28 19:43:42 +0800

    requirements

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     print('KWS Transformers Env Setup')
     print('Installing required packages')
     
-    command = f'pip install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu113 --q'
+    command = f'pip --no-cache-dir install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu113 --q'
     os.system(command)
     
     command = f'pip install -r requirements.txt --q'
